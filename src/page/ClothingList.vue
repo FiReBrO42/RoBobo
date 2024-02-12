cb970bfd820ad4c044aef226399e13dd54df54ce
Sam <sam124674@gmail.com>
2024-02-13 03:18:41 +0800

    內容初步

--- src/page/ClothingList.vue
+++ src/page/ClothingList.vue
@@ -1,5 +1,45 @@
 <template>
-  <div></div>
+  <div>
+    <h2>穿搭靈感</h2>
+    <p>歡迎投稿玩家投稿, 不定期更新。</p>
+  </div>
+  <div>
+    <!-- card -->
+    <ul>
+      <li>
+        <!-- img -->
+        <figure style="width: 281px;height: 354px; ">
+          <img src="@/assets/img/clothingList/clothingList-1.png" alt="">
+        </figure>
+        <!-- content -->
+        <div>
+          <!-- position -->
+          <div>
+            <div>
+              <!-- title -->
+              <p>頭上</p>
+              <!-- name -->
+              <div>
+                <h3>狐狸耳朵</h3>
+                <span>水霧效果</span>
+              </div>
+            </div>
+          </div>
+          <!-- user -->
+          <div>
+            <div>
+              <!-- title -->
+              <p>玩家</p>
+              <!-- name -->
+              <div>
+                <h3>波波麗娜貝貝多</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </li>
+    </ul>
+  </div>
 </template>
 <script setup></script>
 <style lang="scss" scoped></style>
