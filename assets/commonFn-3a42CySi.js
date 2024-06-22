cf6ccde7bf1f70384a075d3c095bd6a96d4ccd68
FiReBrO42 <93040967+FiReBrO42@users.noreply.github.com>
2024-06-22 20:38:31 +0000

    Deploying to gh-pages from @ FiReBrO42/RoBobo@6b9ee3ba113b347a593611768b8fce7509932c4f 🚀

---
+++ assets/commonFn-3a42CySi.js
@@ -0,0 +1,1 @@
+function t(){return{getImage:o=>{try{const r=`/RoBobo/${o}`;return console.log(import.meta.url),new URL(r,import.meta.url).href}catch(r){return console.error("Error loading image:",r),""}}}}export{t as c};
